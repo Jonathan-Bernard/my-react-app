5a0240fe68027f322d1ecb2a6adf4cf45bb34c08
Jonathan-Bernard <johnnyboy59000@gmail.com>
2023-10-10 10:27:39 +0200

    add a pokemon card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,8 @@
 function PokemonCard() {
-    return (
-        <div>
-            <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur artwork"></img>
-            </figure>
-            <figcaption>
-                Bulbasaur
-            </figcaption>
-        </div>
-    );
+    return <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+        <figcaption> Bulbizarre </figcaption>
+    </figure>
 }
 
 export default PokemonCard;
