fb52c4811d8bfa66c8d92f13b967e14f8df9e05c
Jonathan-Bernard <johnnyboy59000@gmail.com>
2023-10-10 12:01:16 +0200

    React 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,27 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+
 function PokemonCard() {
+
+    const pokemon = pokemonList[1]
+
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-            <figcaption> Bulbizarre </figcaption>
-        </figure>
+        <div>
+            <h1>{pokemon.name}</h1>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : '???'}
+        </div>
     );
 }
 
 export default PokemonCard;
+
+
